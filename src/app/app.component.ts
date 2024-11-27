d1e2b2f06dfc4a713b9d1df8eacbf7d4e6c94abb
tonylapoche <terrade.antoine.pro@gmail.com>
2024-11-27 09:05:26 +0100

    en recherche des animations routing

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,36 +1,19 @@
-import {ChangeDetectionStrategy, Component, inject, OnInit, signal} from '@angular/core';
+import {ChangeDetectionStrategy, Component} from '@angular/core';
 import {IonApp, IonRouterOutlet} from "@ionic/angular/standalone";
-import {HeaderComponent} from "./core/layout/header/header.component";
-import {NavigationEnd, Router} from "@angular/router";
 
 @Component({
     selector: 'app-root',
     imports: [
         IonApp,
-        HeaderComponent,
         IonRouterOutlet
     ],
     template: `
-    <ion-app>
-      <app-header [titlePage]="currentRoute()"></app-header>
-          <ion-router-outlet></ion-router-outlet>
-    </ion-app>
-  `,
-    styles: `
-  `,
+        <ion-app>
+            <ion-router-outlet></ion-router-outlet>
+        </ion-app>
+    `,
+    styles: ``,
     changeDetection: ChangeDetectionStrategy.OnPush
 })
-export class AppComponent implements OnInit {
-  currentRoute = signal<string>('');
-  router = inject(Router);
-
-  ngOnInit() {
-    this.router.events.subscribe(event => {
-      if (event instanceof NavigationEnd) {
-        this.currentRoute.set(event.urlAfterRedirects.slice(1));
-        console.log('Route actuelle mise à jour :', event.urlAfterRedirects);
-      }
-    });
-    this.currentRoute.set(this.router.url);
-  }
+export class AppComponent {
 }
--- src/app/core/layout/tabs/tabs.route.ts
+++ src/app/core/layout/tabs/tabs.route.ts
@@ -1,8 +1,8 @@
 import {Routes} from "@angular/router";
 import {TabsPage} from "./tabs.page";
-import aboutRoute from '../../../views/about/about.route';
-import mapRoute from '../../../views/map/map.route';
-import homeRoutes from '../../../views/home/home.route';
+import aboutRoute from '../../../pages/about/about.route';
+import mapRoute from '../../../pages/map/map.route';
+import homeRoutes from '../../../pages/home/home.route';
 
 export const tabsRoute: Routes = [
   {
